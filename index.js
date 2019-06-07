a3decbd5993982180aa28c928c74d2c75693c86a
MitziYolotzin <ultravisuell@hotmail.com>
2019-06-06 19:24:23 -0500

    test burger mondodb

--- index.js
+++ index.js
@@ -12,25 +12,45 @@ const app = express();
 
 
 // Conectar aplicación a MongoDB
-mongoose.connect(mongoUrl, { useNewUrlParser: true });
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true },  (err) => {
+  if (err){
+      return err
+  }
+  console.log("conectado a mongo")
+});
+
+
 
 
 app.set('config', config);
 app.set('pkg', pkg);
 
-
+app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.use(authMiddleware(secret));
 
+//model
+const {Burger} = require('./models/Burger.js')
 
 // Registrar rutas
-routes(app, (err) => {
-  if (err) {
-    throw err;
-  }
+// routes(app, (err) => {
+//   if (err) {
+//     throw err;
+//   }
+// });
+
+
+
+app.get('/burgers', (req, res) => {
+  Burger.find({}, (err, burgers) => {
+      res.send(burgers)
+  })
+  })
 
   // Registro de "middleware" que maneja posibles errores
   app.use(errorHandler);
 
   app.listen(port, () => console.log(`App listening on port ${port}`));
-});
+
+
+
